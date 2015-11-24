6c6f2c123b87d29ee4492c9acea700f500b6ebcb
Muhammad Ghifary <mghifary@gmail.com>
2015-11-25 10:19:49 +1300

    scan ex

--- scan_ex.py
+++ scan_ex.py
@@ -2,37 +2,46 @@ import theano
 import theano.tensor as T
 import numpy as np
 
+# test
+_x = np.asarray([1,4,2,6], dtype='float32')
+_w = 2
+_v = 3
+_u = 1
+
+print(_x)
+
 x = T.vector("inputs", dtype='float32')
 w = T.scalar("w", dtype='float32')
 v = T.scalar("v", dtype='float32')
-# h = T.vector("outputs", dtype='float32')
+u = T.scalar("u", dtype='float32')
 h = T.scalar("outputs", dtype='float32')
 
-def step(x_t, 
+
+
+def step(x_tm1, x_t,
 	h_tm1, 
 	w, v):
-	h_t = w * x_t + v * h_tm1
+	
+	h_t = w * x_t + v * h_tm1 + u * x_tm1
 	return h_t
 
 
-# outputs_info = T.as_tensor_variable(np.asarray(0., x.dtype))
-# outputs_info = T.unbroadcast(alloc_zeros_matrix(X.shape[1], self.output_dim), 1)
+# zero left-padding
+z = T.zeros_like(x)
+y = T.concatenate(([z[-1]], x), axis=0)
+
+
 results, updates = theano.scan(fn=step,
-			sequences=x,
-			outputs_info=h,
+			sequences=[dict(input=y, taps=[-1, -0])],
+			outputs_info=dict(initial=h, taps=[-1]),
 			non_sequences=[w, v]			
 )
 
-rnn = theano.function(inputs=[x, h, w, v], outputs=results[-1])
+rnn = theano.function(inputs=[x, h, w, v, u], outputs=results)
 
-# test
-x = np.asarray([1,4,2,6], dtype='float32')
-w = 2
-v = 3
 
-print(x)
 
-print rnn(x, 0., w, v)
+print rnn(_x, 0., _w, _v, _u)
 
 
 
